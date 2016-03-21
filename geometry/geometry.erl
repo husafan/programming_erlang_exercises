acf3fa3a1afac7bb9d760694819bc70478083f92
Joshua B Harrison <josh.harrison@gmail.com>
2016-03-20 20:41:31 -0700

    Add a method and a test for calculating the area of a circle.

--- geometry/geometry.erl
+++ geometry/geometry.erl
@@ -4,9 +4,14 @@
 test() ->
     12 = area({rectangle, 3, 4}),
     144 = area({square, 12}),
+    12.566370614359172 = area({circle, 2}),
     tests_worked.
 
 area({rectangle, Width, Height}) ->
     Width * Height;
 area({square, Size}) ->
-    Size * Size.
+    Size * Size;
+area({circle, Radius}) ->
+    math:pi() * math:pow(Radius, 2).
+
+
