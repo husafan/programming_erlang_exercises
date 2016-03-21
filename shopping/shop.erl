ef567e9caac3ef2c965a80739c26e7aef7669661
Joshua B Harrison <josh.harrison@gmail.com>
2016-03-20 20:55:33 -0700

    Add a total method that uses costs to sum the toal cost of the shopping list. Also add tests.

--- shopping/shop.erl
+++ shopping/shop.erl
@@ -1,5 +1,10 @@
 -module(shop).
--export([cost/1]).
+-export([test/0, cost/1, total/1]).
+
+test() ->
+    12 = total([{apples, 6}]),
+    67 = total([{orange, 3}, {newspaper, 2}, {pears, 4}]),
+    tests_worked.
 
 cost(orange) -> 5;
 cost(newspaper) -> 8;
@@ -7,3 +12,7 @@ cost(apples) -> 2;
 cost(pears) -> 9;
 cost(milk) -> 7.
 
+total([]) ->
+    0;
+total([{Item, Count}|Tail]) ->
+    cost(Item) * Count + total(Tail).
