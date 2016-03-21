9c4bb02d5e953e93876479002d410d937eb4820c
Joshua B Harrison <josh.harrison@gmail.com>
2016-03-20 22:15:02 -0700

    Add a total_comp method that uses list comprehension to calculate the total cost of a shopping list.

--- shopping/shop1.erl
+++ shopping/shop1.erl
@@ -1,10 +1,12 @@
 -module(shop1).
--export([total/1]).
+-export([total/1, total_comp/1]).
 -import(lists, [map/2, sum/1]).
 
 total(L) ->
     sum(map(fun({Item, Count}) ->
                     shop:cost(Item) * Count end, L)).
 
+total_comp(L) ->
+    sum([shop:cost(Item) * Count || {Item, Count} <- L]).
 
 
