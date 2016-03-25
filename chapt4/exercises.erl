f1fb69219c853a229fcdee3b33042e8557f45cb0
Joshua B Harrison <josh.harrison@gmail.com>
2016-03-24 17:14:32 -0700

    Add a time and date format function for exercise 3.

--- chapt4/exercises.erl
+++ chapt4/exercises.erl
@@ -1,5 +1,5 @@
 -module(exercises).
--export([my_tuple_to_list/1, my_time_func/1]).
+-export([my_tuple_to_list/1, my_time_func/1, my_date_string/1]).
 
 my_tuple_to_list(Tuple) ->
     my_tuple_to_list_helper(Tuple, []).
@@ -15,4 +15,30 @@ my_time_func(Func) ->
     Value = Func(),
     {timer:now_diff(erlang:timestamp(), Start), Value}.
 
+%% Formats a Microsecond value as returned by os:timestamp() as a string.
+%% Example code taken from the os:timestamp man page.
+my_date_string(Timestamp) ->
+    {{Year,Month,Day},{Hour,Minute,Second}} =
+        calendar:now_to_universal_time(Timestamp),
+    MonthString =
+        case Month of
+            1 -> "Jan";
+            2 -> "Feb";
+            3 -> "Mar";
+            4 -> "Apr";
+            5 -> "May";
+            6 -> "Jun";
+            7 -> "Jul";
+            8 -> "Aug";
+            9 -> "Sept";
+            10 -> "Oct";
+            11 -> "Nov";
+            12 -> "Dec"
+        end,
+    io:format("~2w ~s ~4w, ~2w:~2w, ~4w",
+              [Day, MonthString, Year, Hour, Minute, Second]).
+                
+                      
+    
+    
     
